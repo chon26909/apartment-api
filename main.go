9c9524ffe48c0af8f72032fd1c45b8016a71929f
HiChon <59905927+chon26909@users.noreply.github.com>
2024-04-02 22:36:24 +0700

    update main

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chon26909/apartment-api/config"
+	"github.com/chon26909/apartment-api/handlers"
+	"github.com/chon26909/apartment-api/lib"
+	"github.com/chon26909/apartment-api/services"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	config := config.InitConfig()
+
+	fmt.Printf("config %v", config)
+
+	db := lib.NewMySqlConnection(config)
+
+	_ = db
+
+	healthService := services.NewHealthService()
+	healthHandler := handlers.NewHealthHandler(healthService)
+
+	// create router
+	e := echo.New()
+	e.GET("/health", healthHandler.HealthCheck)
+
+	e.Start(fmt.Sprintf(":%d", config.App.Port))
 }
